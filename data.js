0ce138989cfe65ee4eff2d6dbc6bde32f3487001
Danii Bervel <daanielbervelmorales@gmail.com>
2025-06-13 16:27:39 +0200

    Subida inicial del tour 360

---
+++ data.js
@@ -0,0 +1,218 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250613_154952_00_024",
+      "name": "IMG_20250613_154952_00_024",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5237294441615283,
+        "pitch": -0.005737731748830299,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.865493154054686,
+          "pitch": 0.022039976241266857,
+          "rotation": 6.283185307179586,
+          "target": "1-img_20250613_155018_00_025"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.117601272111667,
+          "pitch": 0.9684234286388822,
+          "title": "Django",
+          "text": "The dog"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20250613_155018_00_025",
+      "name": "IMG_20250613_155018_00_025",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7341523030690347,
+        "pitch": 0.018957724645710883,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6225988189942475,
+          "pitch": 0.8591839237109262,
+          "rotation": 0,
+          "target": "0-img_20250613_154952_00_024"
+        },
+        {
+          "yaw": -0.8076687101462294,
+          "pitch": -0.07487326953787132,
+          "rotation": 0,
+          "target": "2-img_20250613_155148_00_026"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20250613_155148_00_026",
+      "name": "IMG_20250613_155148_00_026",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.06850017669130182,
+        "pitch": 0.18927375799100155,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.133088783135193,
+          "pitch": 0.06526826579223588,
+          "rotation": 0,
+          "target": "1-img_20250613_155018_00_025"
+        },
+        {
+          "yaw": 0.38393990846399717,
+          "pitch": 0.14818906752551442,
+          "rotation": 0,
+          "target": "3-img_20250613_155208_00_027"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.31260730965346895,
+          "pitch": 0.5223441587393971,
+          "title": "Django",
+          "text": "The dog"
+        },
+        {
+          "yaw": -1.737710649701821,
+          "pitch": -0.06121049208300633,
+          "title": "Mum",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-img_20250613_155208_00_027",
+      "name": "IMG_20250613_155208_00_027",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.1341170216079135,
+        "pitch": 0.15787375106646095,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7823615833054767,
+          "pitch": 0.21334187804320592,
+          "rotation": 0,
+          "target": "2-img_20250613_155148_00_026"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3752742334876693,
+          "pitch": 0.7257211622933823,
+          "title": "Django",
+          "text": "The dog"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
